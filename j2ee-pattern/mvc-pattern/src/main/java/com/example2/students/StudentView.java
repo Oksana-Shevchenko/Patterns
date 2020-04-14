eddd97ce0271218a2d1cb7148c400356ac8e0f7b
Oksana-Shevchenko <oksana-shevchenko@ukr.net>
2020-04-14 12:43:53 +0300

    J2EE-PT-MVC-002 add example of j2ee pattern

---
+++ j2ee-pattern/mvc-pattern/src/main/java/com/example2/students/StudentView.java
@@ -0,0 +1,10 @@
+package com.example2.students;
+
+public class StudentView {
+
+	public void printStudentDetails(String name, String rollNo) {
+		System.out.println("Student: ");
+		System.out.println("Name: " + name);
+		System.out.println("Roll No: " + rollNo);
+	}
+}
